9cac66a09bf6e5bc29be0fda347cee22cadc0077
Arseny Zarechnev <cyanidesign@gmail.com>
2014-04-13 03:19:36 +0400

    autocomplete-mode, line numbers

--- init.el
+++ init.el
@@ -28,13 +28,26 @@
 (require-package 'evil)
 (require-package 'sr-speedbar)
 (require-package 'monokai-theme)
+(require-package 'auto-complete)
 
 (setq evil-search-module 'evil-search
       evil-want-C-u-scroll t
       evil-want-C-w-in-emacs-state t)
 
 (require 'evil)
+(require 'auto-complete)
 (evil-mode t)
 
 (show-paren-mode t)
 (load-theme 'monokai t)
+
+(setq ac-auto-show-menu t)
+(setq ac-auto-start t)
+(global-auto-complete-mode t)
+(add-to-list 'ac-modes 'lisp-mode)
+
+;; Shut up emacs i'm just starting
+(setq bell-volume 0)
+
+;; Set line count
+(global-linum-mode t)
